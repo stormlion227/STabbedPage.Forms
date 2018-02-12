abcfd21661ac10beeced4d2a765f343494f35e2b
Marko Rothstein <rothstein.marko@yandex.com>
2018-02-13 00:53:02 +0100

    Added empty class.

---
+++ STabbedPage/STabbedPage.Android/STabbedPageRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Stormlion.STabbedPage;
+using Stormlion.STabbedPage.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(STabbedPage), typeof(STabbedPageRenderer))]
+namespace Stormlion.STabbedPage.Droid
+{
+    public class STabbedPageRenderer : TabbedPageRenderer
+    {
+        public STabbedPageRenderer(Context context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ STabbedPage/STabbedPage.iOS/STabbedPageRenderer.cs
@@ -0,0 +1,13 @@
+﻿
+using Stormlion.STabbedPage;
+using Stormlion.STabbedPage.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(STabbedPage), typeof(STabbedPageRenderer))]
+namespace Stormlion.STabbedPage.iOS
+{
+    public class STabbedPageRenderer : TabbedRenderer
+    {
+    }
+}
